22c94cf38368b93ab2dfa29285ed18aea2d0ccf6
Norman <norman.rosenstech@free.fr>
2017-03-05 23:33:49 +0100

    gulp js + bower

---
+++ app/Resources/assets/js/main.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function(){
+    console.log('Un système UNIX boooondieuuu!!');
+    $("h1.bands-title").prepend("<span class='stuff'>Some stuff</span>");
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,75 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var util = require('gulp-util');
-var gulpif = require('gulp-if');
+// Those were used before gulp-plugins addition
+//var sass = require('gulp-sass');
+//var sourcemaps = require('gulp-sourcemaps');
+//var concat = require('gulp-concat');
+//var minifyCSS = require('gulp-minify-css');
+//var util = require('gulp-util');
+//var gulpif = require('gulp-if');
+var plugins = require('gulp-load-plugins')();
 
 var config = {
     assetsDir: 'app/Resources/assets',
+    bowerDir: 'vendor/bower_components',
     sassPattern: 'sass/**/*.scss',
-    production: !!util.env.production, // production true
-    sourcemaps: !util.env.production // production false
+    production: !!plugins.util.env.production, // production true
+    sourcemaps: !plugins.util.env.production // production false
 };
 
-gulp.task('sass', function(){
-    gulp.src(config.assetsDir + '/' + config.sassPattern)
-        .pipe(gulpif(config.sourcemaps, sourcemaps.init())) // Points to original scss file errors in console debugger, not css + gulpif to test env dev or prod
-        .pipe(sass())
-        .pipe(concat('main.css'))
-        .pipe(config.production ? minifyCSS() : util.noop()) // when running gulp --production, minifies the css. Not minified in dev
-        .pipe(gulpif(config.sourcemaps, sourcemaps.write('.'))) // Points to original scss file errors in console debugger, not css
-        .pipe(gulp.dest('web/css'))
+var app = {};
+
+app.addStyle = function( paths, filename ){
+    gulp.src(paths)
+    .pipe(plugins.plumber()) // Avoid gulp to die when there is an error
+    .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.init())) // Points to original scss file errors in console debugger, not css + gulpif to test env dev or prod
+    .pipe(plugins.sass())
+    .pipe(plugins.concat(filename))
+    .pipe(config.production ? plugins.minifyCss() : plugins.util.noop()) // when running gulp --production, minifies the css. Not minified in dev
+    .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.write('.'))) // Points to original scss file errors in console debugger, not css
+    .pipe(gulp.dest('web/css'))
     ;
+}
+
+app.addScript = function( paths, filename ){
+    gulp.src( paths )
+        .pipe(plugins.plumber()) // Avoid gulp to die when there is an error
+        .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.init())) // Points to original scss file errors in console debugger, not css + gulpif to test env dev or prod
+        .pipe(plugins.concat( filename ))
+        .pipe(config.production ? plugins.uglify() : plugins.util.noop()) // when running gulp --production, minifies the css. Not minified in dev
+        .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.write('.'))) // Points to original scss file errors in console debugger, not css
+        .pipe(gulp.dest('web/js'))
+    ;
+}
+
+gulp.task('styles', function(){
+    app.addStyle(
+        [
+            config.bowerDir + '/bootstrap/dist/css/bootstrap.css',
+            config.assetsDir + '/sass/layout.scss',
+            config.assetsDir + '/sass/styles.scss'
+        ], 
+        'main.css'
+    );
+    app.addStyle(
+        [
+            config.assetsDir + '/sass/bands.scss'
+        ], 
+        'bands.css'
+    );
 });
 
-gulp.task('watch', function(){
-    gulp.watch(config.assetsDir + '/' + config.sassPattern, ['sass']);
+gulp.task('scripts', function(){
+    app.addScript([
+        config.bowerDir + '/jquery/dist/jquery.js',
+        config.assetsDir + '/js/main.js'
+    ], 'site.js');
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('watch', function(){    
+    gulp.watch(config.assetsDir + '/' + config.sassPattern, ['styles']);
+    gulp.watch(config.assetsDir + '/js/**/*.js', ['scripts']);
+});
+
+
+gulp.task('default', ['styles', 'scripts', 'watch']);
 
