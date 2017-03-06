e0509439268da75547f21c7733074e7b2ddd2442
Norman <norman.rosenstech@free.fr>
2017-03-06 13:07:08 +0100

    Task order in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,69 +7,138 @@ var gulp = require('gulp');
 //var util = require('gulp-util');
 //var gulpif = require('gulp-if');
 var plugins = require('gulp-load-plugins')();
+var del = require('del');
+var Q = require('q'); // Queuing tool
 
-var config = {
+var config = {    
     assetsDir: 'app/Resources/assets',
     bowerDir: 'vendor/bower_components',
     sassPattern: 'sass/**/*.scss',
     production: !!plugins.util.env.production, // production true
-    sourcemaps: !plugins.util.env.production // production false
+    sourcemaps: !plugins.util.env.production, // production false
+    revManifestPath: 'app/Resources/assets/rev-manifest.json'
 };
 
 var app = {};
 
 app.addStyle = function( paths, filename ){
-    gulp.src(paths)
-    .pipe(plugins.plumber()) // Avoid gulp to die when there is an error
-    .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.init())) // Points to original scss file errors in console debugger, not css + gulpif to test env dev or prod
-    .pipe(plugins.sass())
-    .pipe(plugins.concat(filename))
-    .pipe(config.production ? plugins.minifyCss() : plugins.util.noop()) // when running gulp --production, minifies the css. Not minified in dev
-    .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.write('.'))) // Points to original scss file errors in console debugger, not css
-    .pipe(gulp.dest('web/css'))
+    return gulp.src(paths)
+        // Plumber avoids gulp to die when there is an error
+        .pipe(plugins.plumber())
+        .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.init())) // Points to original scss file errors in console debugger, not css + gulpif to test env dev or prod
+        .pipe(plugins.sass())
+        .pipe(plugins.concat('css/' + filename))
+        .pipe(config.production ? plugins.minifyCss() : plugins.util.noop()) // when running gulp --production, minifies the css. Not minified in dev
+        .pipe(plugins.rev())
+        .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.write('.'))) // Points to original scss file errors in console debugger, not css
+        .pipe(gulp.dest('web'))
+        .pipe(plugins.rev.manifest(config.revManifestPath, { merge: true }))
+        .pipe(gulp.dest('.'))
     ;
-}
+};
 
 app.addScript = function( paths, filename ){
-    gulp.src( paths )
-        .pipe(plugins.plumber()) // Avoid gulp to die when there is an error
+    return gulp.src( paths )        
+        .pipe(plugins.plumber())
         .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.init())) // Points to original scss file errors in console debugger, not css + gulpif to test env dev or prod
-        .pipe(plugins.concat( filename ))
+        .pipe(plugins.concat( 'js/' + filename ))
         .pipe(config.production ? plugins.uglify() : plugins.util.noop()) // when running gulp --production, minifies the css. Not minified in dev
+        .pipe(plugins.rev())
         .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.write('.'))) // Points to original scss file errors in console debugger, not css
-        .pipe(gulp.dest('web/js'))
+        .pipe(gulp.dest('web'))
+        .pipe(plugins.rev.manifest(config.revManifestPath, { merge: true }))
+        .pipe(gulp.dest('.'))
     ;
-}
+};
+
+app.copy = function( srcFiles, outputDir ){
+    return gulp.src(srcFiles)
+        .pipe(gulp.dest(outputDir))
+    ;
+};
 
 gulp.task('styles', function(){
-    app.addStyle(
+    var pipeline = new Pipeline();
+    
+    pipeline.add(
         [
             config.bowerDir + '/bootstrap/dist/css/bootstrap.css',
+            config.bowerDir + '/font-awesome/css/font-awesome.css',
             config.assetsDir + '/sass/layout.scss',
             config.assetsDir + '/sass/styles.scss'
         ], 
         'main.css'
     );
-    app.addStyle(
+    
+    pipeline.add(
         [
             config.assetsDir + '/sass/bands.scss'
         ], 
         'bands.css'
     );
+    
+    return pipeline.run(app.addStyle);
 });
 
 gulp.task('scripts', function(){
-    app.addScript([
+    var pipeline = new Pipeline();
+    
+    pipeline.add([
         config.bowerDir + '/jquery/dist/jquery.js',
         config.assetsDir + '/js/main.js'
     ], 'site.js');
+    
+    return pipeline.run(app.addScript);
 });
 
-gulp.task('watch', function(){    
+gulp.task('fonts', function(){
+    return app.copy( 
+        config.bowerDir + '/font-awesome/fonts/*', 
+        'web/fonts'
+    );
+});
+
+gulp.task('clean', function(){
+    del.sync( config.revManifestPath );
+    del.sync('web/css/*');
+    del.sync('web/js/*');
+    del.sync('web/fonts/*');
+});
+
+gulp.task('watch', function(){     
     gulp.watch(config.assetsDir + '/' + config.sassPattern, ['styles']);
     gulp.watch(config.assetsDir + '/js/**/*.js', ['scripts']);
 });
 
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+gulp.task('default', ['clean', 'styles', 'scripts', 'fonts', 'watch']);
+
+// ***************************************************************************** PIPELINE functions
+
+var Pipeline = function() {
+    this.entries = [];
+};
+Pipeline.prototype.add = function() {
+    this.entries.push(arguments);
+};
+Pipeline.prototype.run = function(callable) {
+    var deferred = Q.defer();
+    var i = 0;
+    var entries = this.entries;
+    var runNextEntry = function() {
+        // see if we're all done looping
+        if (typeof entries[i] === 'undefined') {
+            deferred.resolve();
+            return;
+        }
+        // pass app as this, though we should avoid using "this"
+        // in those functions anyways
+        callable.apply(app, entries[i]).on('end', function() {
+            i++;
+            runNextEntry();
+        });
+    };
+    runNextEntry();
+    return deferred.promise;
+};
 
---
+++ src/AppBundle/Twig/AssetVersionExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Twig;
+
+/**
+ * Description of AssetVersionExtension
+ */
+class AssetVersionExtension extends \Twig_Extension
+{
+    private $appDir;
+    
+    public function __construct($appDir) 
+    {
+        $this->appDir = $appDir;
+    }
+    
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('asset_version', array($this, 'getAssetVersion'))
+        );
+    }
+    
+    public function getAssetVersion($filename)
+    {
+        $manifestPath = $this->appDir . '/Resources/assets/rev-manifest.json';
+        
+        if(!$manifestPath){
+            throw new \Exception(sprintf('Missing manifest file "%s"', $manifestPath));
+        }
+        
+        $assets = json_decode(file_get_contents($manifestPath), true);
+        
+        if(!isset($assets[$filename])){
+            throw new \Exception(sprintf('Missing asset version path for "%s"', $filename));
+        }
+        
+        return $assets[$filename];
+    }
+    
+    public function getName()
+    {
+        return 'asset_version';
+    }
+}
